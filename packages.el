66560e9a0002b6ec772ed07c53cf7bbb21c03838
Michael Neuper <michael@michaelneuper.com>
2024-11-15 14:43:53 +0200

    disable solaire-mode

--- packages.el
+++ packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+(package! solaire-mode :disable t)
+
 (package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
 
 (package! xenops) ; org-latex previews

d5ee350599316a1103a287ddb565528ebef700a4
Michael Neuper <michael@michaelneuper.com>
2024-11-15 14:42:32 +0200

    fix org syntax highlights in babel files

--- packages.el
+++ packages.el
@@ -1,5 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+(package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
+
 (package! xenops) ; org-latex previews
 (package! org-roam-ui)

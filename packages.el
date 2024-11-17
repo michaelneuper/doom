06ac518578ab90b481236a25399de0c6dac20759
Michael Neuper <michael@michaelneuper.com>
2024-11-17 15:47:24 +0200

    fix emacsql not found

--- packages.el
+++ packages.el
@@ -4,6 +4,7 @@
 (package! solaire-mode :disable t)
 
 (package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
+(package! emacsql :pin "491105a")
 
 (package! xenops) ; org-latex previews
 (package! org-roam-ui)

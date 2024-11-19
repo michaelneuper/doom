bf27d82d0509f7601d4c326db336eb49ed1ffba9
Michael Neuper <michael@michaelneuper.com>
2024-11-19 18:08:46 +0200

    solaire mode changes
    re-enable solaire mode
    disable solaire mode in dashboard buffer

--- packages.el
+++ packages.el
@@ -1,8 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! solaire-mode :disable t)
-
 (package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
 (package! emacsql :pin "491105a")
 

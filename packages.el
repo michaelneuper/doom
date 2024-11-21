a9f0890fa762c764021820e659f16ed8c3fb4809
Michael Neuper <michael@michaelneuper.com>
2024-11-21 23:00:07 +0200

    remove emacsql pin

--- packages.el
+++ packages.el
@@ -2,9 +2,10 @@
 ;;; $DOOMDIR/packages.el
 
 (package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
-(package! emacsql :pin "491105a")
 
 (package! xenops) ; org-latex previews
 (package! org-roam-ui)
 (package! copilot
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+(package! copilot-chat
+  :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))

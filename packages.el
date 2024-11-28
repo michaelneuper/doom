801116002254c0b9abfbd9d18cde87c5d0bf579b
Michael Neuper <michael@michaelneuper.com>
2024-11-28 15:49:01 +0200

    add eye candy
    - add and configure spacious-padding
    - disable solaire mode
    - add +pretty flag to org
    - disable current line highlighting
    - change opening size
    - change frame title

--- packages.el
+++ packages.el
@@ -1,6 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+(package! solaire-mode :disable t)
+
 (package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
 
 (package! xenops) ; org-latex previews
@@ -9,5 +11,6 @@
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 (package! copilot-chat
   :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
+(package! spacious-padding)
 (package! eglot-booster
   :recipe (:host github :repo "jdtsmith/eglot-booster"))

abcfb553c5b76aecb1f0e1eb798c6098da7a84d5
Michael Neuper <michael@michaelneuper.com>
2024-11-28 19:00:11 +0200

    switch from xenops to org-latex-preview
    - switch to development branch of org
    - disable xenops
    - reformat packages.el

--- packages.el
+++ packages.el
@@ -1,17 +1,47 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! solaire-mode :disable t)
+;; ORG MODE
+(unpin! org)
 
-(package! org :pin "806abc5a2bbcb5f884467a0145547221ba09eb59")
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+   (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+   :files
+   (:defaults "etc")
+   :build t :pre-build
+   (with-temp-file "org-version.el"
+     (require 'lisp-mnt)
+     (let
+         ((version
+           (with-temp-buffer
+             (insert-file-contents "lisp/org.el")
+             (lm-header "version")))
+          (git-version
+           (string-trim
+            (with-temp-buffer
+              (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+              (buffer-string)))))
+       (insert
+        (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+        (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+        "(provide 'org-version)\n"))))
+  :pin nil)
 
-(package! xenops) ; org-latex previews
 (package! org-roam-ui)
+(package! org-modern)
+
+;; PROGRAMMING
 (package! copilot
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
 (package! copilot-chat
   :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
-(package! spacious-padding)
+
 (package! eglot-booster
   :recipe (:host github :repo "jdtsmith/eglot-booster"))
-(package! org-modern)
+
+;; UI
+(package! solaire-mode :disable t)
+(package! spacious-padding)
+(package! casual)

f706224f9c9eb5132c5b10634496096a5248a585
michaelneuper <michael@michaelneuper.com>
2025-05-26 13:51:21 +0200

    update evil, dired and pdf-tools config
    - enable fine-undo in evil-mode
    - enable dragndrop for dired
    - hide dotfiles by default
    - add pdf keybindings
    - enable midnight-mode by default in pdf's

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2 +dragndrop +pretty) ; organize your plain life in plain text
+       (org +roam2 +dragndrop +gnuplot +pretty) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
